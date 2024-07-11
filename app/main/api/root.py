28fe6b49e3d11e01eab8202d4cc552c849f2c145
ivan <ivan.ivan@karmanow.ru>
2024-07-11 22:19:42 +0700

    Fixed compose.yml and FastAPI root router for deploying in webserver

--- app/main/api/root.py
+++ app/main/api/root.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 
 from app.main.api.task import task_router
 
-root_router = APIRouter()
+root_router = APIRouter(prefix="/api")
 root_router.include_router(task_router)
